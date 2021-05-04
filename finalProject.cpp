dc9139300c88675228098938b2d94d81a6802eb8
Pavan <pash6214@colorado.edu>
2021-05-04 11:08:46 -0600

    Added speaker as a best effort service

--- finalProject.cpp
+++ finalProject.cpp
@@ -117,7 +117,7 @@ using namespace std;
 #define NUM_THREADS (3+1)
 
 #ifdef seqgen
-int freq = 40000000;
+int freq = 20000000;
 #endif
 
 #ifdef seqgen2
@@ -474,10 +474,10 @@ void *Sequencer(void *threadp)
             // Release each service at a sub-rate of the generic sequencer rate
     #ifdef seqgen
             // Servcie_1 = RT_MAX-1	@ 2 Hz
-            if((seqCnt % 4) == 0) sem_post(&semS1);
+            if((seqCnt % 10) == 0) sem_post(&semS1);
 
             // Service_2 = RT_MAX-2	@ 1 Hz
-            if((seqCnt % 5) == 0) sem_post(&semS2);
+            if((seqCnt % 20) == 0) sem_post(&semS2);
             else
             {
                 sem_post(&semS3);
